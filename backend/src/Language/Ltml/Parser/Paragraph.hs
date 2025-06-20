e05304e91ef8f9cf9b09f3358157338e66406e50
Einhard Leichtfuß <alguien@respiranto.de>
2025-06-20 18:25:08 +0200

    ltml: Add general labeling parser
    We removed `:` from the set of acceptable label characters.  This is to
    keep parsing simple, and maybe avoid confusing users.
    
    Also,
     - update docs on label and labeling syntax
     - use labeling parser for paragraphs (addresses #135 w.r.t. paragraphs)

--- backend/src/Language/Ltml/Parser/Paragraph.hs
+++ backend/src/Language/Ltml/Parser/Paragraph.hs
@@ -4,18 +4,21 @@ module Language.Ltml.Parser.Paragraph
 where
 
 import Control.Applicative (optional)
-import Control.Monad.State (evalStateT)
 import Language.Lsd.AST.Type.Paragraph (ParagraphType (..))
 import Language.Ltml.AST.Node (Node (..))
 import Language.Ltml.AST.Paragraph (Paragraph (..))
 import Language.Ltml.Parser (Parser)
 import Language.Ltml.Parser.Label (labelingP)
+import Language.Ltml.Parser.Label (labelingP)
 import Language.Ltml.Parser.Text (textForestP)
+import Text.Megaparsec (try)
+import Text.Megaparsec.Char (char)
 import Text.Megaparsec (try)
 import Text.Megaparsec.Char (char)
 
 -- TODO: Avoid `try`.
+-- TODO: Avoid `try`.
 paragraphP :: ParagraphType -> Parser (Node Paragraph)
-paragraphP (ParagraphType fmt tt) = flip evalStateT True $ do
+paragraphP (ParagraphType fmt tt) = do
     label <- optional $ try $ labelingP <* char '\n'
     Node label . Paragraph fmt <$> textForestP tt
