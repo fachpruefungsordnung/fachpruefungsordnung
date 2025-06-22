6f4e7e9ab1c857ff3b08b1f3427020647778a817
Fab1000 <60816643+Fab1000@users.noreply.github.com>
2025-06-22 14:47:49 +0200

    refactor render API for HTML and plain text

--- backend/src/Server/Handlers/RenderHandlers.hs
+++ backend/src/Server/Handlers/RenderHandlers.hs
@@ -1,75 +1,66 @@
 {-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Server.Handlers.RenderHandlers (RenderAPI, renderServer) where
 
-import Data.Aeson (FromJSON)
+import Data.Aeson (encode)
 import Data.ByteString.Lazy (ByteString)
 import Data.OpenApi
     ( NamedSchema (..)
-    , ToParamSchema
     , ToSchema
     , binarySchema
     , declareNamedSchema
     )
-import Data.Text (Text, lines, toLower, unlines)
-import GHC.Generics (Generic)
+import Data.Text (Text, lines, unlines)
 import Lucid
+import Network.HTTP.Media.MediaType ((//))
 import Servant
 import Servant.Auth.Server
 import Server.Auth (AuthMethod)
 import qualified Server.Auth as Auth
-import Server.HTTPHeaders
 import Server.HandlerUtil
 import Prelude hiding (head, lines, unlines)
 
-type RenderAPI =
-    Auth AuthMethod Auth.Token
-        :> "render"
-        :> Capture "format" OutputFormat
-        :> ReqBody '[JSON] Text
-        :> Post '[OctetStream] (Headers '[HeaderContentType] DocByteString)
-
-renderServer :: Server RenderAPI
-renderServer = renderHandler
-
+-- | Return type for rendered documents
 newtype DocByteString = DocByteString ByteString
 
 instance ToSchema DocByteString where
     declareNamedSchema _ = pure $ NamedSchema (Just "Document BinaryString") binarySchema
 
-instance MimeRender OctetStream DocByteString where
+-- | API type for all render formats
+type RenderAPI =
+    "render"
+        :> ( "html" :> RenderRoute HTML
+                :<|> "plain" :> RenderRoute Plain
+           )
+
+renderServer :: Server RenderAPI
+renderServer = renderHandler renderHTML :<|> renderHandler renderPlain
+
+-- | Format type for HTML
+data HTML
+
+-- | Format type for plain text
+data Plain
+
+-- | MIME type for HTML
+instance Accept HTML where
+    contentType _ = "text" // "html"
+
+-- | MIME type for plain text
+instance Accept Plain where
+    contentType _ = "text" // "plain"
+
+instance MimeRender Plain DocByteString where
     mimeRender _ (DocByteString bs) = bs
 
-data OutputFormat = HTML
-    deriving (Eq, Generic, FromJSON, ToParamSchema)
+instance MimeRender HTML DocByteString where
+    mimeRender _ (DocByteString bs) = bs
 
-formatToHeader :: OutputFormat -> String
-formatToHeader format = case format of
-    HTML -> "text/html" -- PDF -> "application/pdf"
-
-instance FromHttpApiData OutputFormat where
-    parseUrlPiece text = case toLower text of
-        "html" -> Right HTML
-        _ -> Left "Output format is not supported." -- Error msg send if undefined format is requested
-
-renderHandler
-    :: AuthResult Auth.Token
-    -> OutputFormat
-    -> Text
-    -> Handler (Headers '[HeaderContentType] DocByteString)
-renderHandler (Authenticated Auth.Token {..}) format input = case format of
-    HTML ->
-        return $ addHeader (formatToHeader HTML) $ DocByteString $ renderToHTML input
-renderHandler _ _ _ = throwError errNotLoggedIn
-
-renderToHTML :: Text -> ByteString
-renderToHTML input =
+renderHTML :: Text -> ByteString
+renderHTML input =
     let (head, body) = case lines input of
             [] -> ("", "")
             (x : xs) -> (x, unlines xs)
@@ -81,3 +72,18 @@ renderToHTML input =
                     body_ $ do
                         h1_ (toHtml head)
                         p_ (toHtml body)
+
+renderPlain :: Text -> ByteString
+renderPlain = encode
+
+-- | Generic API type for single render format
+type RenderRoute format =
+    Auth AuthMethod Auth.Token
+        :> ReqBody '[JSON] Text
+        :> Post '[format] DocByteString
+
+-- | Generic renderHandler which takes a render function
+renderHandler
+    :: (a -> ByteString) -> AuthResult Auth.Token -> a -> Handler DocByteString
+renderHandler renderFunc (Authenticated _) input = return $ DocByteString $ renderFunc input
+renderHandler _ _ _ = throwError errNotLoggedIn
