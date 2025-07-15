994a62140b0ed77e461287d50e4cd226bf9e33e8
Fab1000 <60816643+Fab1000@users.noreply.github.com>
2025-07-15 23:55:02 +0200

    basic toHtml instances, which ignore most formatting

---
+++ backend/src/Language/Ltml/HTML/HTML.hs
@@ -0,0 +1,107 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-orphans #-}
+
+module Language.Ltml.HTML.HTML () where
+
+import Data.ByteString.Lazy
+import Data.Text
+import Lucid
+
+import Language.Lsd.AST.Format
+import Language.Lsd.AST.Type.Document
+import Language.Lsd.AST.Type.Paragraph
+import Language.Lsd.AST.Type.Section
+import Language.Ltml.AST.Document
+import Language.Ltml.AST.Label
+import Language.Ltml.AST.Node
+import Language.Ltml.AST.Paragraph
+import Language.Ltml.AST.Section
+import Language.Ltml.AST.Text
+
+testAST :: Document
+testAST =
+    Document
+        DocumentFormat
+        DocumentHeader
+        ( DocumentBody
+            [ Node
+                (Just (Label "Heading 1"))
+                ( Section
+                    (SectionFormat (FormatString []))
+                    (Heading (FormatString []) [])
+                    ( Left
+                        [ Node
+                            (Just (Label "Erste Node Label"))
+                            ( Paragraph
+                                (ParagraphFormat (FormatString []))
+                                [ Word "Das ist im Paragraph"
+                                , Word "Ohne Space"
+                                , Space
+                                , Word "Nach dem Space"
+                                , Word "\n"
+                                , Word "Neue Zeile?"
+                                , Styled Bold [Word "Bold"]
+                                , Styled Italics [Word "Italic"]
+                                , Styled Underlined [Word "Underlined"]
+                                ]
+                            )
+                        ]
+                    )
+                )
+            ]
+        )
+
+test :: IO ()
+test = renderToFile "out.html" (html_ $ toHtml testAST)
+
+renderHtml :: Document -> ByteString
+renderHtml document = renderBS $ html_ $ toHtml document
+
+instance ToHtml Document where
+    -- \| builds Lucid 2 HTML from a Ltml Document AST
+    toHtml (Document format header body) = case body of
+        DocumentBody [] -> return ()
+        DocumentBody nodes -> toHtml nodes
+
+instance (ToHtml a) => ToHtml (Node a) where
+    toHtml (Node maybeLabel a) = case maybeLabel of
+        Nothing -> toHtml a
+        Just label -> do
+            h2_ (toHtml label)
+            toHtml a
+
+instance ToHtml Section where
+    toHtml (Section format heading children) = case children of
+        Right cs -> toHtml cs
+        Left cs -> toHtml cs
+
+instance ToHtml Paragraph where
+    toHtml (Paragraph format textTrees) = toHtml textTrees
+
+instance (ToHtmlStyle style) => ToHtml (TextTree style enum special) where
+    toHtml textTree = case textTree of
+        Word text -> toHtml text
+        Space -> toHtml (" " :: Text)
+        Special _ -> toHtml ("Error: Special not supported yet" :: Text)
+        Reference label -> toHtml label
+        Styled style textTrees -> toHtmlStyle style $ toHtml textTrees
+        Enum enum -> toHtml ("Error: Enum not supported yet" :: Text)
+        Footnote _ -> toHtml ("Error: FootNotes not supported yet" :: Text)
+
+class ToHtmlStyle style where
+    toHtmlStyle :: (Monad m) => style -> (HtmlT m a -> HtmlT m a)
+
+instance ToHtmlStyle FontStyle where
+    toHtmlStyle Bold = b_
+    toHtmlStyle Italics = i_
+    toHtmlStyle Underlined = span_ [class_ "underline"]
+
+instance ToHtml Label where
+    toHtml label = toHtml $ unLabel label
+
+instance (ToHtml a) => ToHtml [a] where
+    toHtml [] = mempty
+    toHtml (a : as) = do
+        toHtml a
+        toHtml as
