7a148b98f0bb31548bd271dc55cbf96cb372f93d
merlin <info@bahn.sh>
2025-07-17 19:38:45 +0200

    Konzept für die Versionierung
    Co-authored-by: Finn Evers <dev@bahn.sh>

--- backend/src/DocManV2/Document.hs
+++ backend/src/DocManV2/Document.hs
@@ -1,16 +1,29 @@
-{-# LANGUAGE GADTs #-}
-
 module DocManV2.Document
     ( Document (..)
     , DocumentID (..)
     , Node (..)
     , NodeID (..)
-    , NodeVersion (..)
-    , NodeVersionID (..)
-    ) where
+    , Version (..)
+    , VersionID (..)
+    )
+where
+
+-- Um ein Dokument über die API zu laden, kann ein Baum angefragt werden, der die
+-- Struktur der einzelnen Paragraphen und Knoten darstellt. An einem anderen Endpunkt
+-- können die Inhalte eines bestimmten Paragraphen oder Knoten abgerufen werden.
+-- Mit einem POST auf den gleichen Endpunkt kann der Paragraph oder Knoten bearbeitet
+-- werden. Dabei wird dieser Endpunkt sowohl genutzt, um die Inhalte zu verändern,
+-- als auch um die Struktur zu ändern. Ist die `children` Eigentschaft gesetzt,
+-- so werden die angegebenen Nodes in der gegebenen Reihenfolge fortan als Kindknoten
+-- des bearbeiteten Knotens behandelt. Ist die `Content` Eigentschaft gesetzt,
+-- so wird der Inhalt des Paragraphen (oder Knoten?) geändert.
+-- TODO: Sollen Knoten sowohl Text als auch Kindknoten haben können?
+--       -> Äußerst relevant für interne Darstellung oberhalb des Datalayers und
+--          für die API.
 
 import Data.Text (Text)
 import Data.UUID (UUID)
+import DocManV2.Hash (Hashed)
 import GHC.Int (Int32)
 import UserManagement.Group (GroupID)
 
@@ -36,14 +49,29 @@ data Node = Node
     , nodeKind :: NodeKind
     }
 
-newtype NodeVersionID = NodeVersionID
-    { unNodeVersionID :: Int32
+newtype VersionID = VersionID
+    { unVersionID :: Int32
     }
 
-data NodeVersion
-    = NodeVersion
-    { nodeVersionID :: NodeVersionID
-    , nodeVersionNodeID :: NodeID
-    , nodeVersionContent :: Text
-    , nodeVersionAuthor :: UUID
+data Version
+    = Version
+    { versionID :: VersionID
+    , versionNode :: Node
+    , versionAuthor :: UUID
+    , versionContent :: Hashed Text
+    , versionChildren :: [NodeID]
+    }
+
+data CreateVersion
+    = CreateVersion
+    { createVersionNodeID :: NodeID
+    , createVersionAuthor :: UUID
+    , createVersionContent :: Maybe (Hashed Text)
+    , createVersionChildren :: Maybe [NodeID]
+    }
+
+data Tree = Tree
+    { treeNode :: Node
+    , treeVersionID :: VersionID
+    , treeChildren :: [Tree]
     }
