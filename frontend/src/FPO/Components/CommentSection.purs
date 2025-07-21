71e146a7fe672f2e1c68f2c6030b04018398b7ae
Nummersechs <stu204034@mail.uni-kiel.de>
2025-07-21 12:04:08 +0200

    some fix for commentsection

--- frontend/src/FPO/Components/CommentSection.purs
+++ frontend/src/FPO/Components/CommentSection.purs
@@ -9,7 +9,6 @@ import Effect.Aff.Class (class MonadAff)
 import FPO.Types (Comment, TOCEntry)
 import Halogen as H
 import Halogen.HTML as HH
-import Halogen.HTML.Events as HE
 import Halogen.HTML.Properties as HP
 import Halogen.Themes.Bootstrap5 as HB
 
