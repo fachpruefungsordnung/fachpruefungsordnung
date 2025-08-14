fd69f0381dd3d5364b1fc9175e8bd007a739165f
Fab1000 <60816643+Fab1000@users.noreply.github.com>
2025-08-14 23:01:34 +0200

    backend/html: fix spacing bug regarding styled text
    raw text inside paragraphs is now merged in divs to space them as one block in flex layout

--- backend/src/Language/Ltml/HTML/Test.hs
+++ backend/src/Language/Ltml/HTML/Test.hs
@@ -31,7 +31,7 @@ import System.Directory (removeDirectoryRecursive)
 import Text.Megaparsec (runParser)
 import Prelude hiding (Enum, Word, readFile)
 
-testDoc = readFile "src/Language/Ltml/HTML/Test/studienaufbau_master.txt"
+testDoc = readFile "src/Language/Ltml/HTML/Test/test.txt"
 
 parseTest :: IO ()
 parseTest = do
