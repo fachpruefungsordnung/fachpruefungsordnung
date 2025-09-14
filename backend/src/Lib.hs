19a36c428ed8abdde3d048196f7616b5d32038c8
merlin <info@bahn.sh>
2025-09-14 17:07:46 +0200

    new test document
    Co-authored-by: Φnn Evers <dev@bahn.sh>

--- backend/src/Lib.hs
+++ backend/src/Lib.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( someFunc
     )
 where
 
+import Data.Maybe (fromMaybe)
+import Data.UUID (fromString)
 import Database (getConnection, migrate)
 import Docs (logMessage)
-import Docs.Hasql.Database (run)
+import qualified Docs
+import Docs.ExampleDoc (exampleTree)
+import Docs.Hasql.Database (run, runTransaction)
 import Logging.Logs (Severity (Info))
 import qualified Logging.Scope as Scope
 import Mail (testMail)
@@ -17,9 +23,20 @@ someFunc = do
     Right _ <- migrate connection
     Right _ <-
         flip run connection $
-            logMessage Info Nothing Scope.server "Starting Server..."
+            logMessage Info Nothing Scope.server ("Starting Server..." :: String)
     -- Datenbank zumüllen :))
     testMail
-    -- createTestDocument connection
+    let userID = fromMaybe undefined $ fromString "7f59659a-9a46-4ba0-a911-09698107a6ea"
+    let groupID = 1
+    let title = "Test Document"
+    Right (Right _) <-
+        flip
+            runTransaction
+            connection
+            $ Docs.newDefaultDocument
+                userID
+                groupID
+                title
+                exampleTree
     runServer
     return ()
