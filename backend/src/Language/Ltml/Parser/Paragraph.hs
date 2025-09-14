09734e79abfbf2e08ea3d7ec3eecf9c34f9e05aa
Einhard Leichtfuß <alguien@respiranto.de>
2025-09-14 12:52:58 +0200

    backend/ltml: Permit empty labeled paragraphs
    Previously, a paragraph labeling without an actual paragraph would cause
    a parse error.  Now, it yields an empty labeled paragraph.
    
    The reason for this change was to eliminate that parse error.

--- backend/src/Language/Ltml/Parser/Paragraph.hs
+++ backend/src/Language/Ltml/Parser/Paragraph.hs
@@ -3,23 +3,29 @@ module Language.Ltml.Parser.Paragraph
     )
 where
 
-import Control.Applicative (optional)
+import Control.Applicative (optional, (<|>))
 import Control.Monad.State (evalStateT)
+import Data.Maybe (isJust)
 import Language.Lsd.AST.Type.Paragraph (ParagraphType (ParagraphType))
 import Language.Ltml.AST.Node (Node (Node))
 import Language.Ltml.AST.Paragraph (Paragraph (Paragraph))
 import Language.Ltml.Parser (Parser)
 import Language.Ltml.Parser.Common.Lexeme (nLexeme1)
 import Language.Ltml.Parser.Label (labelingP)
-import Language.Ltml.Parser.Text (ParagraphParser, textForestP)
+import Language.Ltml.Parser.Text (textForestP)
 import Text.Megaparsec (try)
 
 paragraphP :: ParagraphType -> Parser (Node Paragraph)
-paragraphP (ParagraphType fmt tt) =
-    Node
-        -- TODO: Avoid `try`.
-        <$> optional (try $ nLexeme1 labelingP)
-        <*> evalStateT bodyP True
+paragraphP (ParagraphType fmt tt) = do
+    -- Note:
+    --  * Backtracking due to `try` is limited to
+    --    `labelingP <* sp <* char '\n'`, and this is typically cheap.
+    --    - CONSIDER: Avoid `try`.
+    mLabel <- optional (try $ nLexeme1 labelingP)
+    body <-
+        if isJust mLabel
+            then bodyP <|> pure []
+            else bodyP
+    return $ Node mLabel (Paragraph fmt body)
   where
-    bodyP :: ParagraphParser Paragraph
-    bodyP = Paragraph fmt <$> textForestP tt
+    bodyP = evalStateT (textForestP tt) True
