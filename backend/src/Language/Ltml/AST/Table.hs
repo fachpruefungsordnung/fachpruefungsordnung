e48084ca296a6979e8e04215b40b502a24f0dc52
Treebeep <eike.pfeiffer24@gmail.com>
2025-11-04 21:33:30 +0100

    first steps towards variable merged cells

--- backend/src/Language/Ltml/AST/Table.hs
+++ backend/src/Language/Ltml/AST/Table.hs
@@ -12,8 +12,10 @@ module Language.Ltml.AST.Table
     )
 where
 
-import Language.Ltml.AST.Text (TableTextTree, TextTree (Word))
-import Control.Monad.State (State)
+import Language.Ltml.AST.Text (TableTextTree)
+import Control.Monad.State (State, get, put, execState)
+import Control.Monad
+import Data.Array
 
 newtype Table_ = Table_ [Row_]
 
@@ -54,3 +56,79 @@ data Cell = Cell
   , cellRowSpan :: Int
   } deriving (Show)
 
+
+type Matrix a = Array (Int, Int) a
+type Visited = (Int, Int) -> Bool
+type Value = (Int, Int)
+type Position = (Int, Int)
+
+-- Convert a list of lists to an array
+listToArray :: [[a]] -> Matrix a
+listToArray xss =
+    let n = length xss
+        m = if n == 0 then 0 else length (head xss)
+    in array ((0,0), (n-1,m-1))
+             [ ((i,j), (xss !! i) !! j) | i <- [0..n-1], j <- [0..m-1] ]
+
+-- Convert array back to list of lists (optional)
+arrayToList :: Matrix a -> [[a]]
+arrayToList arr =
+    let ((i0,j0),(in_,jn)) = bounds arr
+    in [[ arr ! (i,j) | j <- [j0..jn]] | i <- [i0..in_]]
+
+tableToArray :: Table_ -> Matrix Cell_
+tableToArray (Table_ rows_) =
+    let n = length rows_
+        m = if n == 0 then 0 else length (let (Row_ cells) = rows_ !! 0 in cells)
+    in array ((0,0), (n-1,m-1))
+             [ ((i,j), (rows_ !! i) `getCell` j) | i <- [0..n-1], j <- [0..m-1] ]
+  where
+    getCell (Row_ cells) j = cells !! j
+
+
+-- Your rewritten function
+findEqualSubSquares :: Eq a => Matrix a -> Matrix Value
+findEqualSubSquares matrix =
+    let matrix' = array ((0,0),(n1,m1)) [((i,j), emptyEntry) | i <- [0..n1], j <- [0..m1]]
+        visited0 (_,_) = False
+    in snd $ execState process (visited0, matrix')
+  where
+    ((0,0),(n1,m1)) = bounds matrix
+    n = n1 + 1
+    m = m1 + 1
+    emptyEntry = (0,0)
+
+    updateVisited :: Position -> State (Visited, Matrix Value) ()
+    updateVisited (i, j) = do
+        (visited, mat) <- get
+        let newVisited (x,y) = (y == i && x == j) || visited (x,y)
+        put (newVisited, mat)
+
+    updateMatrix :: Position -> Value -> State (Visited, Matrix Value) ()
+    updateMatrix pos val = do
+        (visited, mat) <- get
+        let newMat = mat // [(pos, val)]
+        put (visited, newMat)
+
+    maxWidth (i, j) val = length $ takeWhile (\x -> x < m && matrix ! (i,x) == val) [j..m-1]
+
+    maxHeight (i, j) w val =
+        length $ takeWhile (\y -> y < n && all (\x -> matrix ! (y,x) == val) [j..j+w-1]) [i..n-1]
+
+    process =
+        forM_ [0..n-1] $ \i ->
+          forM_ [0..m-1] $ \j -> do
+            let pos = (i, j)
+            (visited, _) <- get
+            if visited pos
+                then updateMatrix pos emptyEntry
+                else do
+                    let val = matrix ! pos
+                    let w = maxWidth pos val
+                    let h = maxHeight pos w val
+
+                    forM_ [i..i+h-1] $ \y ->
+                        forM_ [j..j+w-1] $ \x ->
+                            updateVisited (x, y)
+
+                    updateMatrix pos (w,h)
