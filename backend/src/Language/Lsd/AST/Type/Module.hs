49f839d943779ddeec4855cc453c9afaf610ad8f
Fab1000 <60816643+Fab1000@users.noreply.github.com>
2025-11-14 19:39:37 +0100

    backend/ltml: module tables (#713)
    ## Features
    - Syntax for module lists and categorized module lists (see #710)
    - HTML rendering of those tables (also degenerate ones)
    
    ## Issues
    Closes #712 
    
    Syntax:
    Closes #710 
    
    HTML:
    Closes #715

---
+++ backend/src/Language/Lsd/AST/Type/Module.hs
@@ -0,0 +1,22 @@
+module Language.Lsd.AST.Type.Module
+    ( ModuleBlockType (..)
+    , ModuleSchemaType (..)
+    , CategoryType (..)
+    , ModuleType (..)
+    ) where
+
+import Language.Lsd.AST.Common (Keyword)
+import Language.Lsd.AST.Type.Enum (EnumType)
+import Language.Lsd.AST.Type.Text (TextType)
+
+data ModuleBlockType
+    = ModuleBlockType
+        (TextType EnumType)
+        ModuleSchemaType
+        CategoryType
+
+newtype ModuleSchemaType = ModuleSchemaType Keyword
+
+data CategoryType = CategoryType Keyword ModuleType
+
+newtype ModuleType = ModuleType Keyword
---
+++ backend/src/Language/Ltml/Parser/Module.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Language.Ltml.Parser.Module (moduleBlockP, moduleP) where
+
+import Language.Lsd.AST.Common (Keyword)
+import Language.Lsd.AST.Type.Enum (EnumType)
+import Language.Lsd.AST.Type.Module
+    ( CategoryType (..)
+    , ModuleBlockType (..)
+    , ModuleSchemaType (..)
+    , ModuleType (..)
+    )
+import Language.Lsd.AST.Type.Text (TextType)
+import Language.Ltml.AST.Module
+    ( Attribute (..)
+    , Category (..)
+    , Module (..)
+    , ModuleBlock (..)
+    , ModuleSchema (..)
+    , ModuleTable (..)
+    )
+import Language.Ltml.Parser (Parser)
+import Language.Ltml.Parser.Common.Lexeme (lexeme, nLexeme)
+import Language.Ltml.Parser.Keyword (keywordP)
+import Language.Ltml.Parser.Text
+    ( hangingTextP
+    , pipeSeperatedTextForestsP
+    )
+import Text.Megaparsec (choice, many, some, (<?>))
+
+attributeListP :: Keyword -> TextType EnumType -> Parser [Attribute]
+attributeListP kw tt = do
+    nLexeme $ keywordP kw
+    fmap Attribute <$> pipeSeperatedTextForestsP tt
+
+-------------------------------------------------------------------------------
+
+schemaP
+    :: ModuleSchemaType -> TextType EnumType -> Parser ModuleSchema
+schemaP (ModuleSchemaType kw) tt = ModuleSchema <$> attributeListP kw tt <?> "module schema"
+
+moduleP :: ModuleType -> TextType EnumType -> Parser Module
+moduleP (ModuleType kw) tt = Module <$> attributeListP kw tt <?> "module"
+
+categoryP :: CategoryType -> TextType EnumType -> Parser Category
+categoryP (CategoryType kw moduleType) tt = do
+    category <- Attribute <$> nLexeme (hangingTextP kw tt)
+    modules <- many (nLexeme (moduleP moduleType tt))
+    return $ Category category modules
+
+-- | Parse a schema and 0 to n module definitions. The block is terminated by an empty line.
+moduleBlockP
+    :: ModuleBlockType -> Parser ModuleBlock
+moduleBlockP (ModuleBlockType tt schemaType categoryType@(CategoryType _ moduleType)) = do
+    schema <- lexeme (schemaP schemaType tt)
+    groups <- choice moduleTablePs
+    return $ ModuleBlock schema groups
+  where
+    moduleTablePs :: [Parser ModuleTable]
+    moduleTablePs =
+        -- If categorized failes (due to the 'some' requirement), we try plain.
+        -- Thus, empty categorizeds are not allowed
+        [ Categorized <$> some (nLexeme $ categoryP categoryType tt)
+        , Plain <$> many (nLexeme $ moduleP moduleType tt)
+        ]
