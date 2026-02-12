9a0df864e5431d4d71426d1d4915d21780bad1d8
MrSubidubi <dev@bahn.sh>
2026-02-12 18:10:28 +0100

    Cleanup

--- backend/src/Server/Handlers/GroupHandlers.hs
+++ backend/src/Server/Handlers/GroupHandlers.hs
@@ -12,6 +12,7 @@ module Server.Handlers.GroupHandlers
     , groupServer
     ) where
 
+import Control.Monad (forM_, when)
 import Control.Monad.IO.Class
 import Hasql.Connection (Connection)
 import qualified Hasql.Session as Session
@@ -25,6 +26,8 @@ import qualified UserManagement.Sessions as Sessions
 import qualified UserManagement.User as User
 
 import Data.Containers.ListUtils (nubOrd)
+import Data.Maybe (isNothing)
+import UserManagement.Group (GroupPatch (patchDescription))
 import Prelude hiding (readFile)
 
 type GroupAPI =
@@ -60,81 +63,45 @@ createGroupHandler
     :: AuthResult Auth.Token -> Group.GroupCreate -> Handler Group.GroupID
 createGroupHandler (Authenticated token@Auth.Token {..}) (Group.GroupCreate {..}) = do
     conn <- tryGetDBConnection
-    ifSuperOrAnyAdminDo conn token (createGroupAndAddAdmin conn)
-  where
-    createGroupAndAddAdmin :: Connection -> Handler Group.GroupID
-    createGroupAndAddAdmin conn = do
-        eBool <-
-            liftIO $ Session.run (Sessions.checkGroupNameExistence groupCreateName) conn
-        case eBool of
-            Left _ -> throwError errDatabaseAccessFailed
-            Right True -> throwError $ err409 {errBody = "\"A group with that name exists already.\""}
-            Right False -> do
-                eGroupID <-
-                    liftIO $
-                        Session.run (Sessions.addGroup groupCreateName groupCreateDescription) conn
-                case eGroupID of
-                    Left _ -> throwError errDatabaseAccessFailed
-                    Right groupID -> do
-                        addRoleInGroup conn subject groupID User.Admin
-                        mapM_
-                            ( mapM_ (\user -> addRoleInGroup conn user groupID User.Member)
-                                . nubOrd -- deduplicate
-                                . filter (== subject) -- filter admin
-                            )
-                            groupCreateUsers
-                        return groupID
+    ifSuperOrAnyAdminDo conn token $ do
+        exists <- runDB conn $ Sessions.checkGroupNameExistence groupCreateName
+        when exists $
+            throwError $
+                err409 {errBody = "\"A group with that name exists already.\""}
+        groupID <- runDB conn $ Sessions.addGroup groupCreateName groupCreateDescription
+        addRoleInGroup conn subject groupID User.Admin
+        forM_ groupCreateUsers $ \users ->
+            forM_ (nubOrd $ filter (/= subject) users) $ \user ->
+                addRoleInGroup conn user groupID User.Member
+        return groupID
 createGroupHandler _ _ = throwError errNotLoggedIn
 
 -- | If the logged in user is SuperAdmin returns list of all existing groups as
 --   [(GroupID, GroupName)]
 getAllGroupsHandler :: AuthResult Auth.Token -> Handler [Group.GroupOverview]
-getAllGroupsHandler (Authenticated Auth.Token {..}) =
-    if isSuperadmin
-        then do
-            conn <- tryGetDBConnection
-            eGroups <- liftIO $ Session.run Sessions.getAllGroupsOverview conn
-            case eGroups of
-                Left _ -> throwError errDatabaseAccessFailed
-                Right groups -> return groups
-        else throwError errSuperAdminOnly
+getAllGroupsHandler (Authenticated Auth.Token {..}) = do
+    guardSuperAdmin isSuperadmin
+    conn <- tryGetDBConnection
+    runDB conn Sessions.getAllGroupsOverview
 getAllGroupsHandler _ = throwError errNotLoggedIn
 
 getGroupHandler
     :: AuthResult Auth.Token -> Group.GroupID -> Handler Group.Group
 getGroupHandler (Authenticated token) groupID = do
     conn <- tryGetDBConnection
-    ifSuperOrAdminDo conn token groupID (getGroup conn)
-  where
-    getGroup :: Connection -> Handler Group.Group
-    getGroup conn = do
-        eGroupInfo <- liftIO $ Session.run (Sessions.getGroupInfo groupID) conn
-        case eGroupInfo of
-            Left _ -> throwError errDatabaseAccessFailed
-            Right (Group.GroupOverview _ name mDesc) -> do
-                members <- getMembers conn
-                return $ Group.Group groupID name mDesc members
-
-    getMembers :: Connection -> Handler [User.UserInfo]
-    getMembers conn = do
-        eMembers <- liftIO $ Session.run (Sessions.getMembersOfGroup groupID) conn
-        case eMembers of
-            Left _ -> throwError errDatabaseAccessFailed
-            Right members -> return members
+    ifSuperOrAdminDo conn token groupID $ do
+        Group.GroupOverview _ name mDesc <- runDB conn $ Sessions.getGroupInfo groupID
+        members <- runDB conn $ Sessions.getMembersOfGroup groupID
+        return $ Group.Group groupID name mDesc members
 getGroupHandler _ _ = throwError errNotLoggedIn
 
 deleteGroupHandler
     :: AuthResult Auth.Token -> Group.GroupID -> Handler NoContent
 deleteGroupHandler (Authenticated token) groupID = do
     conn <- tryGetDBConnection
-    ifSuperOrAdminDo conn token groupID (deleteGroup conn)
-  where
-    deleteGroup :: Connection -> Handler NoContent
-    deleteGroup conn = do
-        eResult <- liftIO $ Session.run (Sessions.deleteGroup groupID) conn
-        case eResult of
-            Left _ -> throwError errDatabaseAccessFailed
-            Right () -> return NoContent
+    ifSuperOrAdminDo conn token groupID $ do
+        runDB conn $ Sessions.deleteGroup groupID
+        return NoContent
 deleteGroupHandler _ _ = throwError errNotLoggedIn
 
 -- | Updates a group's name and/or description via PATCH
@@ -146,58 +113,42 @@ patchGroupHandler
     -> Handler Group.GroupOverview
 patchGroupHandler (Authenticated token) groupID (Group.GroupPatch {..}) = do
     conn <- tryGetDBConnection
-    ifSuperOrAdminDo conn token groupID (patchGroup conn)
-  where
-    patchGroup :: Connection -> Handler Group.GroupOverview
-    patchGroup conn = do
+    ifSuperOrAdminDo conn token groupID $ do
         -- Validate that at least one field is provided
-        case (patchName, patchDescription) of
-            (Nothing, Nothing) ->
-                throwError $
-                    err400
-                        { errBody =
-                            "\"At least one of 'patchName' or 'patchDescription' must be provided.\""
-                        }
-            _ -> pure ()
+        when (isNothing patchName && isNothing patchDescription) $
+            throwError $
+                err400
+                    { errBody =
+                        "\"At least one of 'patchName' or 'patchDescription' must be provided.\""
+                    }
 
         -- If name is being changed, check for uniqueness
-        case patchName of
-            Just newName -> do
-                eBool <- liftIO $ Session.run (Sessions.checkGroupNameExistence newName) conn
-                case eBool of
-                    Left _ -> throwError errDatabaseAccessFailed
-                    Right True -> do
-                        -- Check if it's the same group (allow keeping same name)
-                        eCurrentInfo <- liftIO $ Session.run (Sessions.getGroupInfo groupID) conn
-                        case eCurrentInfo of
-                            Left _ -> throwError errDatabaseAccessFailed
-                            Right (Group.GroupOverview _ currentName _) ->
-                                if currentName == newName
-                                    then pure ()
-                                    else
-                                        throwError $ err409 {errBody = "\"A group with that name exists already.\""}
-                    Right False -> pure ()
-                -- Update name
-                eNameResult <-
-                    liftIO $ Session.run (Sessions.updateGroupName groupID newName) conn
-                case eNameResult of
-                    Left _ -> throwError errDatabaseAccessFailed
-                    Right () -> pure ()
-            Nothing -> pure ()
+        forM_ patchName $ \newName -> do
+            exists <- runDB conn $ Sessions.checkGroupNameExistence newName
+            when exists $ do
+                -- Check if it's the same group (allow keeping same name)
+                Group.GroupOverview _ currentName _ <-
+                    runDB conn $ Sessions.getGroupInfo groupID
+                when (currentName /= newName) $
+                    throwError $
+                        err409 {errBody = "\"A group with that name exists already.\""}
+            runDB conn $ Sessions.updateGroupName groupID newName
 
         -- Update description if provided
-        case patchDescription of
-            Just newDesc -> do
-                eDescResult <-
-                    liftIO $ Session.run (Sessions.updateGroupDescription groupID newDesc) conn
-                case eDescResult of
-                    Left _ -> throwError errDatabaseAccessFailed
-                    Right () -> pure ()
-            Nothing -> pure ()
+        forM_ patchDescription $ \newDesc ->
+            runDB conn $ Sessions.updateGroupDescription groupID newDesc
 
         -- Return updated group info
-        eGroupInfo <- liftIO $ Session.run (Sessions.getGroupInfo groupID) conn
-        case eGroupInfo of
-            Left _ -> throwError errDatabaseAccessFailed
-            Right groupOverview -> return groupOverview
+        runDB conn $ Sessions.getGroupInfo groupID
 patchGroupHandler _ _ _ = throwError errNotLoggedIn
+
+runDB :: Connection -> Session.Session a -> Handler a
+runDB conn session = do
+    result <- liftIO $ Session.run session conn
+    case result of
+        Left _ -> throwError errDatabaseAccessFailed
+        Right val -> return val
+
+guardSuperAdmin :: Bool -> Handler ()
+guardSuperAdmin True = return ()
+guardSuperAdmin False = throwError errSuperAdminOnly
