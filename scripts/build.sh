92a8d936c981b975b770ddd016fa9acb494f67bb
Merlin <info@bahn.sh>
2026-03-01 23:37:51 +0100

    Scripts: Remote Build (#825)
    # Summary
    
    This PR adds a script which builds all docker images on a remote server
    and loads the built images locally. Thus, it is now possible to build
    and run the project on devices with limited hardware capabilities.
    
    To use the remote build feature, set the `REMOTE_BUILD_HOST` environment
    variable to a valid ssh host and run `scripts/build.sh` in the projects
    root directory, eg:
    
    ```sh
    REMOTE_BUILD_HOST=bahn-sh ./scripts/build.sh
    ```
    
    # Important
    
    With this PR, you **have** to specify a `BUILD_TAG` as shown in the
    `.env.example` in order to build or run the project. If you are using
    the remote build script, make sure to choose a unique tag not used by
    anyone else using the same remote build server! The values `dev` and
    `prod` are reserved for the https://fpo.bahn.sh and
    https://bayonne.informatik.uni-kiel.de deployments.

---
+++ scripts/build.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+CONTEXT="fpo-remote-build"
+
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+LIME_YELLOW=$(tput setaf 190)
+POWDER_BLUE=$(tput setaf 153)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+BRIGHT=$(tput bold)
+NORMAL=$(tput sgr0)
+BLINK=$(tput blink)
+REVERSE=$(tput smso)
+UNDERLINE=$(tput smul)
+
+log() {
+  printf "\n[%s] %s\n" "$(date +'%H:%M:%S')" "$1"
+}
+
+info() {
+  log "[${CYAN}info${NORMAL}] $1"
+}
+
+warn() {
+  log "[${YELLOW}warn${NORMAL}] $1"
+}
+
+if [[ ! -v REMOTE_BUILD_HOST ]]; then
+  warn "'REMOTE_BUILD_HOST' not set. Building locally..."
+  docker compose build
+  exit 0
+fi
+
+info "Creating Context..."
+docker context rm "${CONTEXT}" &> /dev/null || true
+docker context create "${CONTEXT}" \
+  --docker "host=ssh://${REMOTE_BUILD_HOST}"
+
+info "Building on '${REMOTE_BUILD_HOST}'..."
+DOCKER_BUILDKIT=1 docker --context "${CONTEXT}" compose build
+
+info "Loading Images..."
+docker --context ${CONTEXT} compose config --images \
+  | grep fpo \
+  | xargs docker --context "${CONTEXT}" save \
+  | docker load
+
+info "Done!"
